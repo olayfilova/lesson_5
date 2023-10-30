d37f83ab4a4033501ebbd119ce3f06af0cbc2376
sales <ofilova@gmail.com>
2023-10-30 20:20:44 +0000

    adding_for dev_less_2

---
+++ home_wok_5.py
@@ -0,0 +1,84 @@
+# home work
+# val = "yea, I do know"
+# 1 print(val[3:4])
+
+# 2 print(val[-2:-1])
+
+# 3 print(val[:5])
+
+#for letter in val[:-2]:
+   # print(letter)
+
+# print(val[:-2])
+
+# print(str(val[2::2]))
+
+# print(str(val[1::2]))
+
+# print(val[::-1])
+
+# print(val[::-2])
+
+# print(val.__len__())
+
+# val = "yea, I do know it will work and nothing could bother me, ghh. Chat GPT is helping"
+# word = val.split()
+# word_count = len(word)
+# print(word_count)
+
+
+# row = "s"  ####### didn't get
+# crt ="ch"
+# result = crt.find(row) or crt.rfind(row)
+# for crt in row():
+#     if crt() is in row():
+#         print(result)
+#  gpt
+# s = ""
+# ch = input("Введіть символ ch: ")
+#
+# # Ініціалізуємо лічильник кількості знайдених символів
+# count = 0
+#
+# # Починаємо пошук символу ch у рядку s
+# index = s.find(ch)
+#
+# # Використовуємо цикл while для пошуку всіх входжень символу ch
+# while index != -1:
+#     count += 1  # Збільшуємо лічильник
+#     index = s.find(ch, index + 1)  # Продовжуємо пошук, ігноруючи знайдений символ
+#
+# # Виводимо результат
+# print(f"Символ '{ch}' знайдено {count} разів у рядку '{s}'.")
+
+
+
+# s = "Please enter your date of birth"###
+# ch = "e"
+# index = False
+# while True:
+# # index = s.find(ch)
+# # count += 1
+#     index = s.find(ch, index)
+#     if index != -1:
+#         break
+# print(index)
+
+
+
+# str = "eheh his home is his strhhhhh"### мозги не варят,туплю как могу.
+# str_new = str.replace("h", "H")
+# h1 = str.find("h".lower()) and str.rfind("h".lower())
+# # for h1.repl in str_new:
+# # while not h1:
+# # h_1 = False
+# # while h in str_new():
+# #      if h_1 = str_new.find() and str_new.rfind()
+# print(str_new)
+#
+# # for h in srt()
+# #     if str.find(h) and str.rfind(h)
+#
+# # var_str = var.replace("h", "H")
+# # for i in var.str():
+# #      print(var_str)
