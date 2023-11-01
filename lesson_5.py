31f9e21ad69015bd5e25e0b59e976691bae2db17
sales <ofilova@gmail.com>
2023-11-01 19:26:16 +0000

    l-5

---
+++ lesson_5.py
@@ -0,0 +1,51 @@
+# var_str = "Hello"
+# for letter in var_str:
+#     print(letter)
+
+# var_str = "Hello"
+# for letter in var_str:
+#     print(var_str.index(letter))
+
+# var_str = "Hello"
+# for index in range(len(var_str)):
+#     print(index)
+#
+#
+# var_str = "Hello"
+# for index in range(len(var_str)):
+#     print(index, var_str[index])
+
+
+
+######################### enumerate
+# for index, value in enumerate(var_str):
+#     print(index, value)
+#
+#
+# for i in range(10):
+#     print(i)
+
+# val_str ="hello 123456"
+# for letter in val_str:
+#     print(letter)
+
+# val_str = "would not 13"
+# for l in val_str:
+#     if l.isalpha():
+#      print(l)
+
+# val_str = "dsuhg.jpeg"
+# val_str.startwith("d")
+# for i in val_str:
+#     if i.endswith("jpeg"):
+#         print(i)
+
+#  if i.rfind():
+
+
+# val_str.replace()
+
+# val_str = " hello"
+# for i in val_str:
+#     if i.lower() in "aeyou":
+#         print(i)
